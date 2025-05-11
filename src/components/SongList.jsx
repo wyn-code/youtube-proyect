4526a279123e02eef2e51b03d89d23aa4d25cbc2
wyn-code <brunoo6.massocco@gmail.com>
2025-05-11 20:51:07 -0300

    cambios button order

--- src/components/SongList.jsx
+++ src/components/SongList.jsx
@@ -8,7 +8,7 @@ const SongList = ({ songs, viewCounts, handleOpenModal, sortAsc, setSortAsc }) =
           onClick={() => setSortAsc(!sortAsc)}
         >
           <img
-            src="/icons/arrow.png"
+            src="/public/icons/arrow.png"
             alt="orden"
             style={{
               transform: sortAsc ? 'none' : 'rotate(180deg)',
